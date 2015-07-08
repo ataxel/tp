b373765c9e25087cfbd9fef9b8e1fd5aecb3d212
ta.adigo <axelito0>
2015-07-08 17:42:51 +0200

    On templating

--- src/Nova/DemoSiteBundle/Controller/StartController.php
+++ src/Nova/DemoSiteBundle/Controller/StartController.php
@@ -5,6 +5,11 @@ namespace Nova\DemoSiteBundle\Controller;
 use eZ\Publish\API\Repository\Values\Content\Query;
 use Symfony\Component\HttpFoundation\Response;
 use eZ\Bundle\EzPublishCoreBundle\Controller;
+use eZ\Publish\API\Repository\Values\Content\Query\SortClause;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+use eZ\Publish\API\Repository\LocationService;
+use eZ\Publish\API\Repository\Repository;
+
 
 class StartController extends Controller
 {
@@ -17,6 +22,7 @@ class StartController extends Controller
 
     public function rubricDisplayAction($locationId, $viewType, $layout = false, array $params = array())
     {
+
 
 
         $response = $this->get('ez_content')->viewLocation($locationId, $viewType, $layout, $params);
@@ -33,14 +39,51 @@ class StartController extends Controller
      *
      * @param int $locationId
      * @return \Symfony\Component\HttpFoundation\Response
+     *
      */
     public function subItemsAction($locationId)  {
 
+            $query = new Query();
+            $query->criterion = new Criterion\LogicalAnd(
+                array(
+                    new Criterion\ParentLocationId( $locationId ),
+                    new Criterion\ContentTypeIdentifier( 'nv_pkg_version' )
+                )
+            );
+            $result = $this->getRepository()->getSearchService()->findContent($query);
+            $content = array();
+            foreach($result->searchHits as $hit)
+            {
+                $content[] = $hit->valueObject;
+            }
 
-            $location = $this->locationService->loadLocation( $locationId );
-            $childLocationList = $this->loccationService->loadLocationChildren( $location );
 
-            return $this->render( 'NovaDemoSiteBundle:parts:sub_items.html.twig', array("contentList"=>$childLocationList));
+            return $this->render( 'NovaDemoSiteBundle:full:onePackage.html.twig', array("content"=>$content));
+
+    }
+
+    public function rubricContentListAction ($currentLocationId) {
+
+        /*$currentLocation = $this->locationService->loadLocation( $currentLocationId );
+        $childLocationList = $this->locationService->loadLocationChildren( $currentLocation );*/
+        $searchService = $this->getRepository()->getSearchService();
+        $query = new Query();
+        $query->criterion = new Criterion\LogicalAnd(
+
+            array(
+                new Criterion\ParentLocationId($currentLocationId),
+                new Criterion\ContentTypeIdentifier('nv_pkg_version')
+            )
+        );
+
+        $result = $searchService->findContent($query);
+        $content=array();
+        foreach ($result->searchHits as $hit){
+
+            $content[] = $hit->valueObject;
+        }
+
+        return $this->render('NovaDemoSiteBundle:line:line.html.twig', array("content"=>$content));
 
     }
 
